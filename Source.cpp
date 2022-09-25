41836239d8933a24c9a6818449fb4198a8e3c37c
takccc585 <114169693+takccc585@users.noreply.github.com>
2022-09-25 18:41:16 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,125 @@
+﻿#include<iostream>
+using namespace std;
+int main() {
+	/*int a;
+	bool r;
+	cin >> a;
+	r = a > 0;
+	cout << boolalpha << r;*/
+	/*int a;
+	bool r;
+	cin >> a;
+	r = a % 2!=0;
+	cout << boolalpha << r;*/
+	/*int a;
+	bool r;
+	cin >> a;
+	r = a % 2 == 0;
+	cout << boolalpha << r;*/
+	//4
+	/*int a,b;
+	bool r;
+	cin >> a >> b;
+	r = a > 2 && b <= 3;
+	cout << boolalpha << r;*/
+	//5
+	/*int a, b;
+	bool r;
+	cin >> a >> b;
+	r = a >= 0 or b <= -2;
+	cout << boolalpha << r;*/
+	//6
+	/*int a, b, c;
+	bool r;
+	cin >> a >> b >> c;
+	r = a < b and b < c;
+	cout << boolalpha << r;*/
+	//7
+	/*int a, b, c;
+	bool r;
+	cin >> a >> b >> c;
+	r = (a < b and b < c) or (c < b and b < a);
+	cout << boolalpha << r;*/
+	//8
+	/*int a,b;
+	bool r;
+	cin >> a >> b;
+	r = a % 2 != 0 and b % 2 != 0;
+	cout << boolalpha << r;*/
+	//9
+	/*int a,b;
+	bool r;
+	cin >> a >> b;
+	r = a % 2 != 0 or b % 2 != 0;
+	cout << boolalpha << r;*/
+	//10
+	/*int a,b;
+	bool r;
+	cin >> a >> b;
+	r = (a % 2 != 0 and b % 2 == 0) or (a % 2 == 0 and b % 2 != 0);
+	cout << boolalpha << r;*/
+	//11
+	/*int a, b;
+	bool r;
+	cin >> a >> b;
+	r = (a % 2 == 0 and b % 2 == 0) or (a % 2 != 0 and b % 2 != 0);
+	cout << boolalpha << r; */
+	//12
+	/*int a, b,c;
+	bool r;
+	cin >> a >> b >> c;
+	r = a> 0 and b > 0 and c > 0;
+	cout << boolalpha << r; */
+	//13
+	/*int a, b, c;
+	bool r;
+	cin >> a >> b >> c;
+	r = a > 0 or b > 0 or c > 0;
+	cout << boolalpha << r; */
+	//14
+	/*int a, b, c;
+	bool r;
+	cin >> a >> b >> c;
+	r = (a > 0 and b < 0 and c < 0) or (a > 0 and b > 0 and c < 0) or (a < 0 and b < 0 and c > 0);
+	cout << boolalpha << r;*/
+	//15
+	/*int a, b, c;
+	bool r;
+	cin >> a >> b >> c;
+	r = (a > 0 and b > 0 and c < 0) or (a > 0 and b < 0 and c < 0) or (a < 0 and b < 0 and c > 0);
+	cout << boolalpha << r; */
+	//16
+	/*int a;
+	bool r;
+	cin >> a ;
+	r = a % 2 == 0 and a / 10 > 0;
+	cout << boolalpha << r;*/
+	//17
+	/*int a;
+	bool r;
+	cin >> a;
+	r = a % 2 != 0 and a / 100 > 0;
+	cout << boolalpha << r;*/
+	//18
+	/*int a, b, c;
+	bool r;	
+	cin >> a >> b >> c;
+	r = (a==b) or (b==c) or (a==c);
+	cout << boolalpha << r;*/
+	//19
+	/*int a, b, c;
+	bool r;
+	cin >> a >> b >> c;
+	r = (a == -b) or (b == -c) or (a == -c);
+	cout << boolalpha << r;*/
+	//20
+	/*int a, b1, b2, b3;
+	bool r;
+	cin >> a ;
+	b1 = a / 100;
+	b2 = (a % 100) / 10;
+	b3 = (a % 100) % 10;
+	r = b1 != b2 and b1 != b3 and b2 != b3;
+	cout << boolalpha << r;*/
+	return 0;
+}
